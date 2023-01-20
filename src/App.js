5f04664be08b1d6f71161542c7787383c9c4ebe1
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-01-20 11:43:44 +0000

    Setting Up Our Categories

--- src/App.js
+++ src/App.js
@@ -1,42 +1,41 @@
 
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Small Rugs',
+    },
+    {
+      id: 2,
+      title: 'Medium Rugs',
+    },
+    {
+      id: 3,
+      title: 'Large Rugs',
+    },
+    {
+      id: 4,
+      title: 'Cartoon Rugs',
+    },
+    {
+      id: 5,
+      title: 'Modern Rugs',
+    },
+  ];
+
   return (
-    <div className="categories-container"> 
-      <div className="category-container">
-        {/*<img>*/}
-        <div className="category-body-container">
-          <h2>Small Rugs</h2>
-          <p>Shop Now</p>
+    <div className='categories-container'> 
+      {categories.map(({title}) => (
+        <div className='category-container'>
+          <div className='background-image'/>
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
-      <div className="category-container">
-        {/*<img>*/}
-        <div className="category-body-container">
-          <h2>Medium Rugs</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/*<img>*/}
-        <div className="category-body-container">
-          <h2>Large Rugs</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/*<img>*/}
-        <div className="category-body-container">
-          <h2>Modern Rugs</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/*<img>*/}
-        <div className="category-body-container">
-          <h2>Disney Rugs</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      ))}
+      
     </div>
   );
 };
