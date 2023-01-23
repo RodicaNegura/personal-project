3bb81adc9c85eb1ed5d3ce0540784b8b8fabf29b
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-01-23 15:07:51 +0000

    Setting Up User Documents & Firestore Data Models

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,13 @@ import {
   GoogleAuthProvider,
 } from 'firebase/auth';
 
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore';
+
 const firebaseConfig = {
   apiKey: "AIzaSyB4d4lOtj98z7-mbiZESdIKeUrTBfZyqRE",
   authDomain: "awesome-rugs-db.firebaseapp.com",
@@ -26,3 +33,15 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+};
