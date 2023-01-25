2cb47000238a6ecf291ddc4c8abf11f8ff1b10f4
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-01-25 14:18:56 +0000

    Generalizing Form Input Component

--- src/components/sign-up form/sign-up-form.component.jsx
+++ src/components/sign-up form/sign-up-form.component.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 
-import { createAuthWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import FormInput from '../form-input/form-input.component';
+
+import { createAuthWithEmailAndPassword, createUserDocumentFromAuth, } from '../../utils/firebase/firebase.utils';
 
 const defaultFormFields = {
   displayName: '',
@@ -28,7 +30,7 @@ const SignUpForm = () => {
     }
 
     try {  
-      const { user } = await createAuthWithEmailAndPassword(email, password);
+      const { user } = await createAuthWithEmailAndPassword( email, password );
 
       await createAuthWithEmailAndPassword(user, { displayName });
       resetFormFields();
@@ -52,8 +54,8 @@ const SignUpForm = () => {
     <div>
       <h1>Sign up with your email and password</h1>
       <form onSubmit={handleSubmit}>
-        <label>Display Name</label>
-        <input 
+        <FormInput 
+          label="Display Name"
           type='text' 
           required 
           onChange={handleChange} 
@@ -61,8 +63,8 @@ const SignUpForm = () => {
           value={displayName} 
         />
 
-        <label>Email</label>
-        <input 
+        <FormInput 
+          label="Email"
           type='email' 
           required 
           onChange={handleChange} 
@@ -70,8 +72,8 @@ const SignUpForm = () => {
           value={email} 
         />
 
-        <label>Password</label>
-        <input 
+        <FormInput
+          label="Password" 
           type='password' 
           required 
           onChange={handleChange} 
@@ -79,8 +81,8 @@ const SignUpForm = () => {
           value={password} 
         />
 
-        <label>Confirm Password</label>
-        <input 
+        <FormInput 
+          label="Confirm Password"
           type='password' 
           required 
           onChange={handleChange} 
