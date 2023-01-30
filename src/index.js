abed1c6c7196a2658bf3c4d2817bae9747652446
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-01-30 12:58:01 +0000

    Toggle Cart Open

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
 import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 
 import './index.scss';
 
@@ -16,9 +17,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-      <ProductsProvider>
-        <App />
-      </ProductsProvider>       
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>       
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
